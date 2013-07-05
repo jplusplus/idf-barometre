63993e23299a5ceb2220d45fa7644e360b9a57aa
Pierre Romera <hello@pirhoo.com>
2013-07-05 14:38:26 +0200

    Added redirection for seemless refactoring #20

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.simple import redirect_to
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',    
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),    
     url(r'^barometre/', include('app.barometre.urls')),
+    url(r'^/$', redirect_to, {'url' : '/barometre/'})
 )
